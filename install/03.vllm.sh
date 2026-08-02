106d2ec411f04d603f49d9015fcb210381301cb2
Mehmet Pekmezci <mehmet.pekmezci.32@gmail.com>
2026-08-02 21:34:46 +0300

    MP

--- install/03.vllm.sh
+++ install/03.vllm.sh
@@ -2,8 +2,11 @@
 ## you may also select a release from there and install the image by its version.
 
 
-# sudo docker pull vllm/vllm-openai:v0.25.1
+# sudo docker pull vllm/vllm-openai:v0.26.0
 sudo docker pull vllm/vllm-openai:latest
+
+sudo docker run --rm --entrypoint pip vllm/vllm-openai:latest show vllm
+sudo docker inspect --format='{{index .Config.Labels "org.opencontainers.image.version"}}' vllm/vllm-openai:latest
 
 echo
 echo
--- services/01.vllm.sh
+++ services/01.vllm.sh
@@ -8,6 +8,10 @@ then
 fi
 
 
+sudo docker run --rm --entrypoint pip vllm/vllm-openai:latest show vllm
+sudo docker inspect --format='{{index .Config.Labels "org.opencontainers.image.version"}}' vllm/vllm-openai:latest
+
+
 DOCKER_NAME=VLLM_0
 sudo docker inspect $DOCKER_NAME &>/dev/null
 if [ $? = 0 ]
@@ -15,23 +19,43 @@ then
     sudo docker start $DOCKER_NAME 
 else
 #sudo docker run --name $DOCKER_NAME --runtime nvidia --restart=always --gpus all \
-sudo docker run --name $DOCKER_NAME --runtime nvidia --gpus all \
+
+echo sudo docker run --name $DOCKER_NAME --runtime nvidia --gpus all \
   -v $AGENT_INFRA_MODELS_DIR/$AGENT_INFRA_MODEL:/local_model \
-  --enable-auto-tool-choice --tool-call-parser $AGENT_INFRA_MODEL_TOOL_CALL_PARSER \
   -p 8000:8000 \
   --ipc=host \
   --env "HF_HUB_OFFLINE=1" \
   --env "TRANSFORMERS_OFFLINE=1" \
   vllm/vllm-openai:latest \
   --model /local_model \
+  --enable-auto-tool-choice --tool-call-parser $AGENT_INFRA_MODEL_TOOL_CALL_PARSER \
+  --gpu-memory-utilization 0.85 \
+  --dtype float16 \
+  --max-model-len 32000 
+
+sudo docker run --name $DOCKER_NAME --runtime nvidia --gpus all \
+  -v $AGENT_INFRA_MODELS_DIR/$AGENT_INFRA_MODEL:/local_model \
+  -p 8000:8000 \
+  --ipc=host \
+  --env "HF_HUB_OFFLINE=1" \
+  --env "TRANSFORMERS_OFFLINE=1" \
+  vllm/vllm-openai:latest \
+  --model /local_model \
+  --enable-auto-tool-choice --tool-call-parser $AGENT_INFRA_MODEL_TOOL_CALL_PARSER \
   --gpu-memory-utilization 0.85 \
   --dtype float16 \
   --max-model-len 32000 >& $AGENT_INFRA_LOG_DIR/vllm.log &
+
+
+#sudo docker run --name VLLM_0 --runtime nvidia --gpus all -v /home/atalet/workspace/agent-infra/models/Qwen/Qwen2.5-Coder-1.5B-Instruct:/local_model -p 8000:8000 --ipc=host --env HF_HUB_OFFLINE=1 --env TRANSFORMERS_OFFLINE=1 vllm/vllm-openai:latest   --model /local_model  --enable-auto-tool-choice --tool-call-parser hermes --gpu-memory-utilization 0.85 --dtype float16 --max-model-len 32000
+
+
 
   ## max-model-len 32000 : context len is 32K
 
 fi
 
+exit
 
 WAIT_FOR_STARTUP=1
 
